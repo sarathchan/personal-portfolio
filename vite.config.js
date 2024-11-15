1add36ec8e2ee8774e999eb4c53d3603d2c3aa8a
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-11-15 16:09:16 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,35 @@ export default defineConfig({
   },
   build: {
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,    // removes console.logs
+        drop_debugger: true,   // removes debugger statements
+        pure_funcs: ['console.log'] // removes console.log specifically
+      },
+      format: {
+        comments: false        // removes comments
+      }
+    },
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
         },
+        // Additional optimization options
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
+    sourcemap: false,  // disable sourcemaps in production
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096, // 4kb
   },
 })
-
