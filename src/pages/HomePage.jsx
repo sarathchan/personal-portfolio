319a02f2f12603c447406aedcc7f8fc49ea5e0cf
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-11-27 00:55:42 +0530

    add banner in website

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import ScrollStack from "../components/common/ScrollStack";
 import SEO from "../components/SEO";
+import CollapsibleBanner from "../components/common/Banner";
 
 const HomePage = ({ setActiveSection }) => {
   return (
@@ -11,6 +12,7 @@ const HomePage = ({ setActiveSection }) => {
         description="Welcome to my portfolio website. I'm a Full Stack Developer specializing in modern web technologies."
         path="/"
       />
+      <CollapsibleBanner />
       <Hero />
       <ScrollStack onSectionChange={setActiveSection} />
     </div>
