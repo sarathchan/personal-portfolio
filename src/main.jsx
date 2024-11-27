111f4ced240a4e6f9a75bc176862221d0229697d
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-11-27 11:37:18 +0530

    remove vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
-import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-    <BrowserRouter>
-      <App />
-      <Analytics />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
 )
